2a411c4fcd1d591de2ecfcf4fd393476ac1274c9
Kazz <mmarena@seznam.cz>
2022-07-09 21:21:21 +0200

    macos support

--- include/cg_main_e.h
+++ include/cg_main_e.h
@@ -1,8 +1,14 @@
 #ifndef CG_MAIN_E_H
 #define CG_MAIN_E_H
 
+#if __APPLE__
+void init_hud(void);
+void update_hud(void);
+void draw_hud(void);
+#else
 void __wrap_init_hud(void);
 void __wrap_update_hud(void);
 void __wrap_draw_hud(void);
+#endif
 
 #endif //CG_MAIN_E_H
--- src/cg_main_e.c
+++ src/cg_main_e.c
@@ -5,19 +5,31 @@ extern void __real_init_hud(void);
 extern void __real_update_hud(void);
 extern void __real_draw_hud(void);
 
+#if __APPLE__
+void init_hud(void)
+#else
 void __wrap_init_hud(void)
+#endif
 {
     __real_init_hud();
     init_accel();
 }
 
+#if __APPLE__
+void update_hud(void)
+#else
 void __wrap_update_hud(void)
+#endif
 {
     __real_update_hud();
     update_accel();
 }
 
+#if __APPLE__
+void draw_hud(void)
+#else
 void __wrap_draw_hud(void)
+#endif
 {
     __real_draw_hud();
     draw_accel();
